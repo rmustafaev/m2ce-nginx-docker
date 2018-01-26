9994028e2463ca49e73cb6a4e6d05d27b0c98fde
Rinat Mustafaev <rmustafaev@setronica.com>
2018-01-26 14:25:38 +0700

    M2CE-4 Add ssl certificate into magento setup Let`s encrypt solution were used for that purpose and acme-nginx tool https://github.com/kshcherban/acme-nginx

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,19 @@
 FROM nginx:1.12
 
 #Copy configuration and operation files
-COPY ./conf/* /etc/nginx/
-COPY ./bin/start.sh /usr/local/bin/
+COPY . /tmp/
+
+RUN rm -f /etc/nginx/conf.d/* && \
+  mv /tmp/bin/* /usr/local/bin/ && \
+  mv /tmp/conf/nginx.conf /etc/nginx/ && \
+  mv /tmp/conf/conf.d/mage.conf /etc/nginx/conf.d/ && \
+  apt-get update && apt-get install -y cron \
+  python-openssl \
+  python-crypto \
+  python-setuptools \
+  python-pip && \
+  pip install acme-nginx && \
+  apt-get clean
 
 WORKDIR /srv/www
 
--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-[ ! -z "${PHP_HOST}" ]                 && sed -i "s/PHP_HOST/${PHP_HOST}/" /etc/nginx/conf.d/mage.conf
-[ ! -z "${PHP_PORT}" ]                 && sed -i "s/PHP_PORT/${PHP_PORT}/" /etc/nginx/conf.d/mage.conf
-[ ! -z "${APP_MAGE_MODE}" ]            && sed -i "s/APP_MAGE_MODE/${APP_MAGE_MODE}/" /etc/nginx/conf.d/mage.conf
-[ ! -z "${WWW_DIR}" ]                  && sed -i "s~WWW_DIR~${WWW_DIR}~" /etc/nginx/conf.d/mage.conf
+DOMAIN_NAME=${DOMAIN_NAME:-localhost}
 
+sed -i "s|PHP_HOST|${PHP_HOST:-phpfpm}|" /etc/nginx/conf.d/mage.conf
+sed -i "s|PHP_PORT|${PHP_PORT:-9000}|" /etc/nginx/conf.d/mage.conf
+sed -i "s|APP_MAGE_MODE|${APP_MAGE_MODE:-default}|" /etc/nginx/conf.d/mage.conf
+sed -i "s|DOMAIN_NAME|${DOMAIN_NAME}|" /etc/nginx/conf.d/mage.conf
+sed -i "s|WWW_DIR|${WWW_DIR:-/srv/www}|" /etc/nginx/conf.d/mage.conf
+
+echo -e "Domain is ${DOMAIN_NAME}"
+
+#if [[ $DOMAIN_NAME != localhost* ]]; then
+
+#Generate certificate via let`s encrypt and renew it every 15th of month
+#/usr/local/bin/acme-nginx -d ${DOMAIN_NAME}
+
+#cat <<EOF | crontab -u root -
+#30 2 15 * * /usr/local/bin/acme-nginx -d ${DOMAIN_NAME} && service nginx reload >> /var/log/letsencrypt.log 2>&1
+#EOF
+
+#else 
+#  echo -e "No need to generate and use SSL certificate for ${DOMAIN_NAME}"
+#fi
+
+# Start the cron service
+/usr/sbin/cron
+
+# Start the nginx service
 /usr/sbin/nginx -g "daemon off;"
+
