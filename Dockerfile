ed7d5eff23cd518b2e90acaa4c1d98a90803b15c
Rinat Mustafaev <rmustafaev@setronica.com>
2018-01-26 14:25:38 +0700

    M2CE-4 Add ssl certificate into magento setup Let`s encrypt solution were used for that purpose and acme-nginx tool https://github.com/kshcherban/acme-nginx

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,11 @@ FROM nginx:1.12
 #Copy configuration and operation files
 COPY ./conf/* /etc/nginx/
 COPY ./bin/start.sh /usr/local/bin/
+RUN apt-get update && apt-get install -y python-openssl \
+  python-crypto \
+  python-setuptools \
+  python-pip && \
+  pip install acme-nginx
 
 WORKDIR /srv/www
 
--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,14 @@
 [ ! -z "${PHP_HOST}" ]                 && sed -i "s/PHP_HOST/${PHP_HOST}/" /etc/nginx/conf.d/mage.conf
 [ ! -z "${PHP_PORT}" ]                 && sed -i "s/PHP_PORT/${PHP_PORT}/" /etc/nginx/conf.d/mage.conf
 [ ! -z "${APP_MAGE_MODE}" ]            && sed -i "s/APP_MAGE_MODE/${APP_MAGE_MODE}/" /etc/nginx/conf.d/mage.conf
+[ ! -z "${DOMAIN_NAME}" ]              && sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/" /etc/nginx/conf.d/mage.conf
 [ ! -z "${WWW_DIR}" ]                  && sed -i "s~WWW_DIR~${WWW_DIR}~" /etc/nginx/conf.d/mage.conf
+
+#Generate certificate via let`s encrypt and renew it every 15th of month
+/usr/local/bin/acme-nginx -d ${DOMAIN_NAME} -d www.${DOMAIN_NAME}
+
+cat <<EOF | crontab -u -
+30 2 15 * * /usr/local/bin/acme-nginx -d ${DOMAIN_NAME} -d www.${DOMAIN_NAME} >> /var/log/letsencrypt.log 2>&1
+EOF
 
 /usr/sbin/nginx -g "daemon off;"
